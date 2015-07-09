d2efe5261bde73bd993d5e369e5649e4b4375d95
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-08 22:56:25 -0400

    adds fixtures

--- test/test.js
+++ test/test.js
@@ -11,21 +11,39 @@ var fs = require('fs');
 var should = require('should');
 var gitignore = require('./');
 
-function read(fp) {
-  return fs.readFileSync('fixtures/' + fp, 'utf8');
-}
-
 describe('gitignore', function () {
   it('should parse a gitignore file and return an array:', function () {
-    gitignore(read('a.txt')).should.containDeep(['temp', 'tmp', 'TODO.md', 'vendor']);
+    gitignore('fixtures/a.txt').should.containDeep([
+     'temp',
+      'tmp',
+     'TODO.md',
+     'vendor'
+    ]);
   });
 
   it('should not include code comments:', function () {
-    gitignore(read('c.txt')).should.containDeep(['*.orig', '*.out', '*.pid', '*.rej', '*.seed', '*.swo', '*.sw']);
-    gitignore(read('c.txt')).should.not.containDeep(['# OS or Editor folders']);
+    gitignore('fixtures/c.txt').should.containDeep([
+     '*.orig',
+     '*.out',
+     '*.pid',
+     '*.rej',
+     '*.seed',
+     '*.swo',
+     '*.sw'
+    ]);
+    gitignore('fixtures/c.txt').should.not.containDeep(['# OS or Editor folders']);
   });
 
   it('should uniquify the results when an array of patterns is passed:', function () {
-    gitignore(read('d.txt'), ['d']).should.eql(['a', 'b', 'c', 'd']);
+    gitignore('fixtures/d.txt', ['d']).should.eql([
+     'a/*',
+     'a',
+     'b/*',
+     'b',
+     'c/*',
+     'c',
+     'd/*',
+     'd'
+    ]);
   });
 });
