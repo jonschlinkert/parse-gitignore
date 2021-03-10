fd499020b6f58ea194e830bc9a3a17a6019a5a0b
Alexis Puga <alexispuga@pm.me>
2021-03-10 08:10:29 -0600

    Update usage examples for gitignore.stringify()
    Use async/await.

--- examples/stringify.js
+++ examples/stringify.js
@@ -3,9 +3,9 @@ const parse = require('..');
 const { stringify } = parse;
 const str = fs.readFileSync('.gitignore', 'utf8');
 
-parse(str).then((res) => {
+parse(str).then(async (res) => {
   console.log(res);
-  console.log(stringify(res.sections));
+  console.log(await stringify(res.sections));
 }).catch((error) => {
   console.error(error);
 });
